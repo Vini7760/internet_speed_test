ecc0d286d18b703e2d32b4f66d5aee379ab4fc4e
Vini7760 <65764828+Vini7760@users.noreply.github.com>
2024-06-14 12:57:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from gui import SpeedTestApp
-import tkinter as tk
+import matplotlib
+matplotlib.use('Agg')
+from speed_test import test_speed
+import utils.logger as logger
 
 def main():
-    root = tk.Tk()
-    app = SpeedTestApp(root)
-    root.mainloop()
+    print("Starting speed test...")
+    speed_results = test_speed()
+    if speed_results:
+        logger.log_speed_test_results(speed_results)
+        print("Speed test completed and results logged.")
+    else:
+        print("Speed test failed.")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-    
